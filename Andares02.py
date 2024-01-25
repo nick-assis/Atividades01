75e4e2f0a738cfdb1b27fdb7029603b99b0e3f98
Nícolas de Assis <nicolasassis@ciandt.com>
2024-01-24 22:05:19 -0300

    primeiro commit

---
+++ Andares02.py
@@ -0,0 +1,8 @@
+contador = i
+
+while (contador <= 20):
+    if (contador == 13):
+        contador = contador + 1
+        continue 
+    else 
+    print(contador)
---
+++ Andares03.py
@@ -0,0 +1,7 @@
+for i in range(20, 0, -1):
+    if (i == 13):
+        continue
+    else:
+        print(i)
+    
+
